dcff90263109590c75359646a764dee0efeb0ddb
karan147369 <103363950+karan147369@users.noreply.github.com>
2023-07-07 02:16:07 +0530

    nodemon

--- routing/index.js
+++ routing/index.js
@@ -1,35 +1,26 @@
 const express = require("express");
 const routing = express.Router();
-const { verifyDoctor } = require('../controller');
-
+const controller = require("../controller");
 
 routing.get("/doctors/login", async (req, res, next) => {
-    const response = await verifyDoctor(req.username, req.password);
+  const response = await controller.verifyDoctor(req.username, req.password);
 });
 
 routing.post("/doctors/register", async (req, res, next) => {
-    const response = await registerDoctor();
+  const response = await controller.registerDoctor();
 });
 
 routing.post("/patients/register", async (req, res, next) => {
-    const response = await registerPatient();
+  const response = await controller.registerPatient();
 });
 routing.get("/patients/:id/create_report", async (req, res, next) => {
-    const response = await createReport();
+  const response = await controller.createReport();
 });
 
 routing.get("/patients/:id/all_reports", async (req, res, next) => {
-    const response = await allReports();
+  const response = await controller.allReports();
 });
 
-routing.get("/reports/:status", async (req, res, next) => {
-
-});
-
-
-
-
-
-
+routing.get("/reports/:status", async (req, res, next) => {});
 
 module.exports = routing;
