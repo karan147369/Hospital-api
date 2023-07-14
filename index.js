75e69a73d774fca8101f9385b64fd1be138b3733
karan147369 <103363950+karan147369@users.noreply.github.com>
2023-07-15 02:59:52 +0530

    env changes

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const cors = require("cors");
 const app = express();
 const route = require("./routing");
 const bodyParser = require("body-parser");
+const port = process.env.PORT;
 
+console.log(process.env);
 app.use(bodyParser.json());
 app.use("/", route);
 app.use(cors);
-app.listen(4000);
+app.listen(port, "0.0.0.0");
